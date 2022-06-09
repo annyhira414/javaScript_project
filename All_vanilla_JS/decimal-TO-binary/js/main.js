8c6e7f8526d96ea40a7763866a67ce320f07be1f
annyhira414 <annyhira@gmail.com>
2022-06-09 21:15:56 +0600

    Decimal to Binary Converter

---
+++ All_vanilla_JS/decimal-TO-binary/js/main.js
@@ -0,0 +1,25 @@
+const result = document.getElementById("result");
+const submit = document.getElementById("submit");
+
+function converter(e){
+    e.preventDefault();
+    const number = document.getElementById("number").value;
+    
+    if (number === ''){
+        alert("Enter Your Number");
+    }
+    else if (number < 0 ){
+        alert("Enter positive Number")
+    }
+    else{
+        result.style.visibility = 'visible';
+    }
+
+     binaryNumber = Number(number).toString(2);
+     result.innerHTML = binaryNumber;
+
+  
+    
+}
+
+submit.addEventListener("click", converter);
